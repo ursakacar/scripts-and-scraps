ce938be11b8ab85c20c949523d8aa4a083cbbd22
Ursa Kacar <kacar.ursa@gmail.com>
2020-06-30 16:20:37 +0200

    adds ability to specify branch

--- translation-testing/setup-test-translations.sh
+++ translation-testing/setup-test-translations.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+read -p 'Enter branch name: ' uibranch
+
 mkdir abpui-translations
 cd abpui-translations
 git clone git@gitlab.com:eyeo/adblockplus/abpui/adblockplusui.git
 cd adblockplusui
+git checkout $uibranch
 npm install
 npm run dist
 npm start
