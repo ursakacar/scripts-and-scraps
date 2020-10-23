ae75c17f4b4375ec916e1ef8bb21b6061bbddacb
Ursa Kacar <kacar.ursa@gmail.com>
2020-10-23 19:17:45 +0200

    adds a results file instead of echoing

--- notification-distribution/notification-distribution.sh
+++ notification-distribution/notification-distribution.sh
@@ -6,9 +6,10 @@
 
 ##################
 # vars that need to be specified:
-numberOfRequests=5000
+numberOfRequests=7000
 notificationId=8
-sourcefile=notifications-versions-file
+sourceFile=notifications-versions-file
+resultsFile=04_ui-860-activate-20-reach-50__results.txt
 setDists=(0.01 0.14 0.15 0.20 0.20 0.15 0.15)
 ##################
 
@@ -16,27 +17,32 @@ for i in $(seq $numberOfRequests); do
 	curl -k --insecure --progress-bar "https://notification.local/notification.json" | grep 'version\|links';
 done > notifications-versions-file
 
-allRequests=$(cat $sourcefile | wc -l)
+allRequests=$(cat $sourceFile | wc -l)
 
-echo 'all requests: '$allRequests
-echo
+(echo;
+echo 'PATCH: ' $resultsFile;
+echo 'all requests: '$allRequests) > $resultsFile
+
+# how many % had links
+ 	eval "notificationsInSample=$(cat $sourceFile | grep -c "links")"
+ 	eval "notificationDistribution=$(echo "scale=2 ;  $notificationsInSample / $numberOfRequests" | bc)"
+	(echo 'number of notifications: '$notificationsInSample;
+	echo 'notification distribution: '$notificationDistribution;
+	echo '_____________';
+	echo) >> $resultsFile
 
 # distribution of each variant
 for i in ${!setDists[@]}; do
 	#we need X for grepping version pattern that starts with 1
 	x=$((i+1))
  	eval sampleVersionPattern="'$notificationId/$x\"'"
- 	eval "requestsPerVersion=$(cat $sourcefile | grep -c "$sampleVersionPattern")"
+ 	eval "requestsPerVersion=$(cat $sourceFile | grep -c "$sampleVersionPattern")"
  	eval "actualVersionDist=$(echo "scale=4 ; $requestsPerVersion / $numberOfRequests" | bc)"
 
- 	echo 'number of requests for version: '$x 'with pattern' $sampleVersionPattern ':' $requestsPerVersion
-	echo 'set version distribution: '${setDists[$i]}
-	echo 'actual distribution: '$actualVersionDist
-	echo
+ 	(echo 'number of requests for version: '$x 'with pattern' $sampleVersionPattern ':' $requestsPerVersion;
+	echo 'set version distribution: '${setDists[$i]};
+	echo 'actual distribution: '$actualVersionDist;
+	echo) >> $resultsFile
  done
 
-# how many % had links
- 	eval "notificationsInSample=$(cat $sourcefile | grep -c "links")"
- 	eval "notificationDistribution=$(echo "scale=2 ;  $notificationsInSample / $numberOfRequests" | bc)"
-	echo 'number of notifications: '$notificationsInSample
-	echo 'notification distribution : '$notificationDistribution
+cat $resultsFile
