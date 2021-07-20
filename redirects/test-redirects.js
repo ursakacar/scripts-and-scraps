de1ba315ecff7b0414d51ee0bbb449194b80ae1e
Ursa Kacar <kacar.ursa@gmail.com>
2021-07-20 19:27:02 +0200

    adds basic info comment in the script

--- redirects/test-redirects.js
+++ redirects/test-redirects.js
@@ -1,8 +1,19 @@
+/*
+This script is used for validating that the redirects from https://gitlab.com/eyeo/specs/spec/-/blob/master/spec/adblockplus.org/documentation-link.md work
+
+As I use it sporadically I do not keep it up-to-date so some links might be outdated.
+
+Data
+doc-redirect-links.json : the 'main' file with majority of links
+doc-language-links.json : I used this for testing the link resolution for different languages in an effort to keep the main file at least a little decluttered :)
+doc-problem-links,json : links that fail for various reasons and I never bothered to take a look what's going on, but still wanted to keep track of them.
+*/
+
 const https = require('https')
 const fs = require('fs')
 
 const baseUrl = 'https://adblockplus.org/redirect?link='
-// const redirectLinksString = fs.readFileSync('doc-language-links.json')
+// change doc-redirect-links to whichever file you want the script to use
 const redirectLinksString = fs.readFileSync('doc-redirect-links.json')
 const redirectLinks = JSON.parse(redirectLinksString)
 
