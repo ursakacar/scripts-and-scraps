8a006ee2e7c787b7dc8d73779daf4d3e0d4dc6bb
Ursa Kacar <kacar.ursa@gmail.com>
2021-07-20 19:18:58 +0200

    adds script for testing redirects

--- redirects/test-redirects.js
+++ redirects/test-redirects.js
@@ -1,48 +1,32 @@
 const https = require('https')
-const http = require('http')
 const fs = require('fs')
 
 const baseUrl = 'https://adblockplus.org/redirect?link='
+// const redirectLinksString = fs.readFileSync('doc-language-links.json')
 const redirectLinksString = fs.readFileSync('doc-redirect-links.json')
 const redirectLinks = JSON.parse(redirectLinksString)
 
 redirectLinks.forEach((redirectLink) => {
-    let areRedirectsSuccessful = true
-    const sourceUrl = baseUrl + redirectLink.source
-    checkUrlRedirect(sourceUrl, redirectLink.destination).catch((responseString) => {
-        console.log(`${redirectLink.source} ${responseString} \n`)
-        areRedirectsSuccessful = false
-    })
-
-    return areRedirectsSuccessful ? 0 : 1
+  const sourceUrl = baseUrl + redirectLink.source
+  checkUrlRedirect(sourceUrl, redirectLink.destination).then((responseString) => {
+    console.log(`${redirectLink.source} ${responseString} \n`)
+  }).catch((responseString) => {
+    console.log(`${redirectLink.source} ${responseString} \n`)
+  })
 })
 
 function checkUrlRedirect(source, destination) {
-    return new Promise((resolve, reject) => {
-        const protocol = (source.startsWith('https://') ? https : http)
-        protocol.get(source, (response) => {
-            if (response.statusCode > 300 && response.statusCode < 400) {
-                const redirectLocation = getAbsoluteRedirectLocation(response.headers.location, source)
-                resolve(checkUrlRedirect(redirectLocation, destination))
-            } else {
-                if ( source === destination && response.statusCode === 200) {
-                    resolve(`PASSED`)
-                } else {
-                    reject(`FAILED \n-> Redirect failed at ${source} with response code: ${response.statusCode} \n-> Expected destination ${destination}`)
-                }
-            }
-                
-        })
+  return new Promise((resolve, reject) => {
+    https.get(source, (response) => {
+      if (response.statusCode > 300 && response.statusCode < 400) {
+        resolve(checkUrlRedirect(response.headers.location, destination))
+      } else {
+        if ( source === destination && response.statusCode === 200) {
+          resolve(`PASSED`)
+        } else {
+          reject(`FAILED \n-> Redirect failed at ${source} with response code: ${response.statusCode} \n-> Expected destination ${destination}`)
+        }
+      }
     })
-}
-
-function getAbsoluteRedirectLocation(redirectLocation, source) {
-    if (redirectLocation.startsWith('https://') || redirectLocation.startsWith('http://')) {
-        return redirectLocation
-    }
-
-    const sourceUrl = new URL(source)
-    const sourceProtocol = sourceUrl.protocol
-    const sourceHostname = sourceUrl.hostname
-    return `${sourceProtocol}//${sourceHostname}${redirectLocation}`
+  })
 }
